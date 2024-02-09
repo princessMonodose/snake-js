01d06f5c03eea596682277cae3afec5d3c0884cc
princessmonodose <casem991@gmail.com>
2024-02-09 11:55:17 +0100

    snake can't turn 180 unaliving itself

--- script.js
+++ script.js
@@ -127,16 +127,24 @@ function handleKeyPress(event) {
   } else {
     switch (event.key) {
       case "ArrowUp":
-        direction = "up";
+        if (direction != "down") {
+          direction = "up";
+        }
         break;
       case "ArrowDown":
-        direction = "down";
+        if (direction != "up") {
+          direction = "down";
+        }
         break;
       case "ArrowLeft":
-        direction = "left";
+        if (direction != "right") {
+          direction = "left";
+        }
         break;
       case "ArrowRight":
-        direction = "right";
+        if (direction != "left") {
+          direction = "right";
+        }
     }
   }
 }
